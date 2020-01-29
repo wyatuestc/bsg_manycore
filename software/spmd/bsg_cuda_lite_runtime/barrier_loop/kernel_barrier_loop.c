0b74dedb60fd08582ec9b5ed2584db41e9163f06
Seyed Borna Ehsani <behsani@cs.washington.edu>
2020-01-28 19:59:49 -0800

    Added a barrier loop cuda lite kernel

---
+++ software/spmd/bsg_cuda_lite_runtime/barrier_loop/kernel_barrier_loop.c
@@ -0,0 +1,26 @@
+//This kernel performs a barrier among all tiles in tile group 
+
+#include "bsg_manycore.h"
+#include "bsg_set_tile_x_y.h"
+
+
+#define BSG_TILE_GROUP_X_DIM bsg_tiles_X
+#define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
+#include "bsg_tile_group_barrier.h"
+INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
+
+
+int  __attribute__ ((noinline)) kernel_barrier_loop(int N) {
+
+	bsg_cuda_print_stat_kernel_start();
+
+	for (int i = 0; i < N; i ++) {
+		bsg_cuda_print_stat_start(1);
+		bsg_tile_group_barrier(&r_barrier, &c_barrier);
+		bsg_cuda_print_stat_end(1);
+	}
+
+	bsg_cuda_print_stat_kernel_end();
+
+return 0;
+}
